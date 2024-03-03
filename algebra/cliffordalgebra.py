e188d80d144759cd025b837df5ab79ad6a2971f4
rockey-math <112349400+rockey-math@users.noreply.github.com>
2024-03-02 23:11:56 -0500

    Update cliffordalgebra.py

--- algebra/cliffordalgebra.py
+++ algebra/cliffordalgebra.py
@@ -109,12 +109,13 @@ class CliffordAlgebra(nn.Module):
         mv[..., s] = tensor
         return mv
 
-    def embed_grades(self, mv: torch.Tensor, tensor: torch.Tensor, /, grade: int) -> torch.Tensor: # 🛑➕
+    def embed_grades(self, mv: torch.Tensor, tensor: torch.Tensor, /, grade: int, tensor_index: torch.Tensor) -> torch.Tensor: # 🛑➕
         #mv = torch.zeros(*tensor.shape[:-1], 2**self.dim, device=tensor.device)
         ##with torch.device(tensor.device):
         #    mv = tensor 
         s = self.grade_to_slice[grade]  
         mv[..., s] = tensor
+        mv[..., tensor_index] = mv
         return mv
         
     def get(self, mv: torch.Tensor, blade_index: tuple[int]) -> torch.Tensor:
